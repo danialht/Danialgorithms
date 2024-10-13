53f310f3f04fc93120a765c0d8f8a1d5cec99ef6
Danial-Hosseintabar <danial.ht10@gmail.com>
2024-10-12 22:29:18 -0400

    DFS added

---
+++ DFS.py
@@ -0,0 +1,17 @@
+class DFS:
+    def search(self, start_node, representation_function = lambda x : x):
+        traversal = []
+        
+        def recursive_search(node):
+            if node is not None:
+                traversal.append(node)
+            else:
+                return
+            if node.get_left_child() is not None:
+                recursive_search(node.get_left_child())
+            if node.get_right_child is not None:
+                recursive_search(node.get_right_child())
+        
+        recursive_search(start_node)
+        
+        return traversal
